dfac731e9b76acb9e7fbbc127f2c6b00bc9600ce
bherreras <bherreras@minsait.com>
2022-10-24 11:22:31 +0200

    se añade template

--- app-loyal/src/app/app.module.ts
+++ app-loyal/src/app/app.module.ts
@@ -6,12 +6,28 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SaldoComponent } from './components/saldo/saldo.component';
+import { DatosEquipoComponent } from './components/datos-equipo/datos-equipo.component';
+import { ContactoClienteComponent } from './components/contacto-cliente/contacto-cliente.component';
+import { RecomendacionesComponent } from './components/recomendaciones/recomendaciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    TopbarComponent,
+    FooterComponent,
+    SaldoComponent,
+    DatosEquipoComponent,
+    ContactoClienteComponent,
+    RecomendacionesComponent
   ],
   imports: [
     BrowserModule,
